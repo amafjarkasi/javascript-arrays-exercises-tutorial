d61b91cd0ef906c30b68f7f4e7e6f328ae1e2300
amafjarkasi <amafjarkasi@gmail.com>
2020-10-01 07:55:21 +0000

    newest update

--- exercises/06.4-Loop-from-the-half-to-the-end/app.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/app.js
@@ -1,10 +1,14 @@
 var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-var initialValue = 0;
-var stopValue = 0;
+var initialValue = Math.floor((mySampleArray.length - 1) / 2);
+
+console.log(initialValue);
+
+var stopValue = mySampleArray.length - 1;
+
 var increasingValue = 1;
 
-for(var i = initialValue; i<stopValue; i = i + increasingValue)
-{
+for(var i = initialValue; i < stopValue; i++) {
+    console.log(initialValue);
 	console.log(mySampleArray[i]);
 }
