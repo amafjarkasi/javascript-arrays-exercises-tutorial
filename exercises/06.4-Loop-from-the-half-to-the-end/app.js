869622b4f5788bb84a1d185dfa212e12b631dee8
amafjarkasi <amafjarkasi@gmail.com>
2020-10-01 08:26:18 +0000

    g

--- exercises/06.4-Loop-from-the-half-to-the-end/app.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/app.js
@@ -1,14 +1,11 @@
 var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-var initialValue = Math.floor((mySampleArray.length - 1) / 2);
+const initialValue = Math.floor((mySampleArray.length + 1) / 2);
 
-console.log(initialValue);
-
-var stopValue = mySampleArray.length - 1;
+const stopValue = mySampleArray.length - 1;
 
 var increasingValue = 1;
 
-for(var i = initialValue; i < stopValue; i++) {
-    console.log(initialValue);
+for(var i = initialValue; i <= stopValue; i++) {
 	console.log(mySampleArray[i]);
 }
--- exercises/06.5-One-last-looping/app.js
+++ exercises/06.5-One-last-looping/app.js
@@ -1,3 +1,8 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 //your code here
+const arrayEnd = mySampleArray.length - 1;
+
+for (var counter = arrayEnd; counter >= 0; counter--) {
+    console.log(mySampleArray[counter]);
+}
